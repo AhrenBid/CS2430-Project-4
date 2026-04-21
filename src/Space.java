bbbcd286258041ff7e311e4400bf8a7ad885dbb1
ZakButters <buttezac003@gmail.com>
2026-04-20 20:04:38 -0600

    Create Space.java

---
+++ src/Space.java
@@ -0,0 +1,43 @@
+public class Space {
+    private String name;
+    private String color;
+    private String type;
+    private Player owner;
+    private int visitCount;
+
+    public Space(String name, String color, String type) {
+        this.name = name;
+        this.color = color;
+        this.visitCount = 0; // Upon creation the visit count is at 0
+        this.type = type;
+    }
+
+    public void incrementVisitCount() {
+        visitCount++;
+    }
+
+    public void action(Player player) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getVisitCount() {
+        return visitCount;
+    }
+        
+}
