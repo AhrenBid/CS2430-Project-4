89acd6c498d9991493bf21c5623657ee68c57daf
AhrenBid <156833384+AhrenBid@users.noreply.github.com>
2026-05-01 13:51:20 -0600

    Update Dice.java

--- src/Dice.java
+++ src/Dice.java
@@ -21,29 +21,44 @@ public class Dice
         dice2 = 0;
         rand = new Random();
     }
-    
+
+    /**
+        roll the dice
+    **/
     public void roll()
     {
         dice1 = rand.nextInt(5) + 1;
         dice2 = rand.nextInt(5) + 1;
         isDouble = dice1 == dice2;
     }
-    
+
+    /**
+        return the value of dice 1 on last roll
+    **/
     public int getDice1()
     {
         return dice1;
     }
-    
+
+    /**
+        return the value of dice 2 on the last roll
+    **/
     public int getDice2()
     {
         return dice2;
     }
-    
+
+    /**
+        return the total value of the dice
+    **/
     public int getTotal()
     {
         return dice1 + dice2;
     }
-    
+
+    /**
+        return whether or not the last roll was a double
+    **/
     public boolean isDouble()
     {
         return isDouble;
